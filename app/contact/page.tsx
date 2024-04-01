2107cb2eedc8cff43906c669aa077554110a157c
loandre <uloandre@gmail.com>
2024-04-01 14:26:25 -0300

    in progress

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,11 +10,11 @@ export default function Contact() {
   const [message, setMessage] = useState<string>('');
 
   const handlePhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
-    let value = e.target.value.replace(/\D/g, ''); // Remove tudo que não é número
+    let value = e.target.value.replace(/\D/g, ''); 
     if (value.length > 2 && value.length <= 6) {
       value = `(${value.slice(0, 2)}) ${value.slice(2)}`;
     } else if (value.length > 6) {
-      value = `(${value.slice(0, 2)}) ${value.slice(2, 6)}-${value.slice(6, 10)}`;
+      value = `(${value.slice(0, 2)}) ${value.slice(2, 6)}-${value.slice(6, 11)}`;
     }
     setPhoneNumber(value);
   };
